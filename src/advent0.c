dbf13c9119190acd38b1cf4a661cf2b326e93747
derpicknicker1 <10minutemail@web.de>
2018-11-28 01:48:44 +0100

    Changed template

--- src/advent0.c
+++ src/advent0.c
@@ -3,10 +3,13 @@ PLACE INSTRUCTIONS HERE
 */
 
 #include "advent.h"
-#define DAY 0
 
 static char *input = NULL;
 
+
+/*##########################
+# Get input data from file #
+##########################*/
 static int getInput(char *f) {
 	char * line = NULL;
     size_t l = 0;
@@ -17,42 +20,50 @@ static int getInput(char *f) {
 		return 0;
 	}
 	
-	while( getline(&line, &l, file) != -1 ) {
+	// This is a example for reading ONE line from file.
+	// Change it to the needed method (multiline, nodes, etc.)
+	if( getline(&line, &l, file) != -1 ) {
 		input = (char*)realloc(input, strlen(line) + 1);
 		strcpy(input,line);
 	}
+	else 
+		return 0;
 
 	free(line);
 	fclose(file);
 	return 1;
 }
 
+
+/*##########################
+# Function to solve part A #
+##########################*/
 void get0a(char * f) {
 
 	if(!getInput(f))
 		return;
 
-	/*
-		Process data for part A
-	*/
+	/* Process data for part A here */
+
+	printf("0a: %s\n", input);
 
 	free(input);
 	input = NULL;
-
-	printf("0a: %s\n", "Example 0a");
 }
 
+
+/*##########################
+# Function to solve part B #
+##########################*/
 void get0b(char *f) {
 
 	if(!getInput(f))
 		return;
 
-	/*
-		Process data for part B
-	*/
+	/* Process data for part B here */	
+	
+	printf("0b: %s\n\n", input);
 
 	free(input);
-	input = NULL;	
-	
-	printf("0b: %s\n\n", "Example 0b");
+	input = NULL;
 }
