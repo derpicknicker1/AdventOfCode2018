b3c17285587309777ed8a41df0ea1b61cbb1790f
derpicknicker1 <10minutemail@web.de>
2018-12-05 00:04:10 +0100

    Solution 4

--- main.c
+++ main.c
@@ -11,12 +11,12 @@ clock_t begin;
 void getTime(int i) {
 	double timeSpent = (double)(clock() - begin) / CLOCKS_PER_SEC;
 	begin = clock();
-	printf("Runtime %d: %.3f\n\n", i, timeSpent);
+	printf("Runtime %d: %.3f\n\n\n", i, timeSpent);
 }					
 
 int main() {
 	
-	printf("\n\n### Solutions ###\n\n");
+	printf("\n\n#################\n### Solutions ###\n#################\n\n");
 
 	clock_t beginAll = clock();
 	begin = clock();
@@ -25,8 +25,8 @@ int main() {
 	execute(1)
 	execute(2) 
 	execute(3)
-	/* execute(4)
-	execute(5)
+	execute(4)
+	/* execute(5)
 	execute(6)
 	execute(7)
 	execute(8)
